e3c13f2d5d69697b551e974be1388ef717ca1cea
Muhammad Hasan Septiadi <septiadi@as-MacBook-Pro.local>
2017-07-17 15:13:20 +0700

    fix logic atomic empty ranges without comment

--- js/block.js
+++ js/block.js
@@ -130,7 +130,6 @@ function getSections(
 * Function to check if the block is an atomic entity block.
 */
 function isAtomicEntityBlock(block: Object): boolean {
-  // if (isEmptyString(block.text) ||
   if ((block.entityRanges.length > 0 && isEmptyString(block.text)) ||
     (block.type === 'atomic') && block.entityRanges.key) {
     return true;
@@ -511,7 +510,6 @@ export function getBlockMarkup(
 ): string {
   const blockHtml = [];
   if (isAtomicEntityBlock(block)) {
-    // let atomicRangesKey = (block.entityRanges.hasOwnProperty("0") && block.entityRanges[0].hasOwnProperty("key"))?block.entityRanges[0].key:0;
     blockHtml.push(
       getEntityMarkup(
         entityMap,
